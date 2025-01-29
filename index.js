f265f9454c3ddd5cf2671965aea6d13dd9413e3f
samad13 <yusufabdulsamad93@gmail.com>
2025-01-29 22:03:42 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(cors());
+
+app.use(cors({
+    origin: 'localhost5173',
+}));
+
+
+app.get('/', (req, res) => {
+    const email = 'yusufabdulsamad93@gmail.com'; 
+    const current_datetime = new Date().toISOString(); 
+    const github_url = 'https://github.com/samad13/repo'; 
+
+    const response = {
+        email,
+        current_datetime,
+        github_url,
+    };
+
+    res.status(200).json(response);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
