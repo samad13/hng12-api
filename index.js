26d03baff49a5812a19ed7a116b80bc5e1fc90d9
samad13 <yusufabdulsamad93@gmail.com>
2025-01-29 22:07:19 +0100

    adding my repo

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.use(cors({
-    origin: 'localhost5173',
+    origin: 'localhost:5173',
 }));
 
 
 app.get('/', (req, res) => {
     const email = 'yusufabdulsamad93@gmail.com'; 
     const current_datetime = new Date().toISOString(); 
-    const github_url = 'https://github.com/samad13/repo'; 
+    const github_url = 'https://github.com/samad13/hng12-api'; 
 
     const response = {
         email,
